b53bfbeb296ba8a974a6a7583b4b97bc6b7dfd23
Anthony Mora <crepystories1@gmail.com>
2024-11-11 14:44:22 -0400

    Arreglando error en settings

--- Web_App/settings.py
+++ Web_App/settings.py
@@ -38,7 +38,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cuerpo-bomberos.onrender.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'bomberos-production.onrender.com']
 
 
 
