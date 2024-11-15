a066a9d8ee852b72b92ddda0af13e360680dfee0
Anthony Mora <crepystories1@gmail.com>
2024-11-15 11:43:23 -0400

    Haciendo collectstatic

---
+++ staticfiles/js/loader-function.js
@@ -0,0 +1,50 @@
+let loadingActive = false;
+
+async function fetchWithLoader(url, options = {}) {
+  try {
+    const loadingScreen = document.getElementById("loadingScreen");
+    if (loadingScreen) {
+      loadingScreen.style.display = "block";
+      loadingActive = true;
+    }
+
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(
+        `Error en la solicitud: ${response.status} ${response.statusText}`
+      );
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Error al consumir la API:", error);
+
+    // Revisa si `response` está definido antes de arrojar el error
+    const responseStatus = error.response?.status || "sin definir";
+    const responseText = error.response?.statusText || "sin definir";
+    throw new Error(`Error en la API: ${responseStatus} ${responseText}`);
+  } finally {
+    const loadingScreen = document.getElementById("loadingScreen");
+    if (loadingScreen && loadingActive) {
+      loadingScreen.style.display = "none";
+      loadingActive = false;
+    }
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("form").forEach((form) => {
+    form.addEventListener("submit", function () {
+      const loadingScreen = document.getElementById("loadingScreen");
+      if (loadingScreen) {
+        loadingScreen.style.display = "block";
+      }
+    });
+  });
+
+  window.addEventListener("beforeunload", function () {
+    const loadingScreen = document.getElementById("loadingScreen");
+    if (loadingScreen) {
+      loadingScreen.style.display = "block";
+    }
+  });
+});
