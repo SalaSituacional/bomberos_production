7bde3761dab6b4c857a2cf49294ff34ae1075c8a
EJCM05 <ghack7756@gmail.com>
2025-07-22 15:05:55 -0400

    solucion de ven911 graficas

--- ven911/views.py
+++ ven911/views.py
@@ -5,22 +5,20 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
 from django.http import JsonResponse
-from datetime import datetime, timedelta
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from django.db.models import Max, Count, Case, When, Q, Sum
 from django.utils.timezone import localdate
 from django.forms.models import model_to_dict
 from django.views.decorators.http import require_http_methods
-from datetime import time
 from .urls import *
 from .forms import *
 from django.db.models import Prefetch
 from openpyxl import Workbook
 import json
 from openpyxl.styles import Font, Alignment
-from datetime import timedelta # Importa timedelta para operaciones de fecha
 from collections import defaultdict # Para agrupar y sumar fácilmente
+from datetime import datetime, date, time, timedelta # <-- Agrega esta línea
 
 # Vista del dashboard Ven911
 @login_required
