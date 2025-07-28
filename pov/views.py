8e3415db07b0cc4f166300f3b2036abf25e0754a
EJCM05 <ghack7756@gmail.com>
2025-07-28 15:54:48 -0400

    carga de nuevo usuario y formulario pov

---
+++ pov/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+
+def Dashboard_pov(request):
+    user = request.session.get('user')
+    if not user:
+            return redirect('/')
+
+    return render(request, "pov/dashboardPov.html", {
+        "user": user,
+        "jerarquia": user["jerarquia"],
+        "nombres": user["nombres"],
+        "apellidos": user["apellidos"],
+    })
+
